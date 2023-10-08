06d5afab2ead2600c03c25b1b20e350546ea6ff6
Henok <henokmicrometre@gmail.com>
2023-10-08 16:49:28 +0100

    react props lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
 
-export default App;
+
+const List = (props) => {
+    return (
+      <>
+    <p>{props.tasks.join(" , ")}</p>
+      </>
+    )
+    
+
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        <List tasks={["Walk", "Cook", "Bake"]} />
+        <h2>Tomorrow</h2>
+        <List tasks={["Study", "Code", "Eat"]} />
+        <h2>Tomorrow</h2>
+      </div>
+    );
+  }
+};
+
+
+
+export default ToDo
